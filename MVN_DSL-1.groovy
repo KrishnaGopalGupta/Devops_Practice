190d8aa19261dbb5362e41ae12692b1836c73be4
Krishna Gopal Gupta <krishnagupta0225@gmail.com>
2020-12-24 15:59:08 +0530

    second updated DSL commit

--- MVN_DSL-1.groovy
+++ MVN_DSL-1.groovy
@@ -2,7 +2,7 @@ job('MVN-DSL-1'){
     description("This is my first DSL script $(new Date()) to creating the Jobs")
     scm{
 
-        git('https://github.com/KrishnaGopalGupta/Devops_Practice.git','jenkinsDSL')
+        git('https://github.com/KrishnaGopalGupta/Devops_Practice.git','MVN-1')
     }
 
     triggers{
